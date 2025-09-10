24bcd79d37c05c28f32f6ee0cabeaf3154f9ebee
GT4933 <nairgovind06@gmail.com>
2025-09-10 21:14:40 +0530

    Update main.ino

--- frimware/esp32-devkit/main.ino
+++ frimware/esp32-devkit/main.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
-
+#define BLYNK_TEMPLATE_ID "TMPL3Hy5PI7Ve"
+#define BLYNK_TEMPLATE_NAME "Drowsiness Detection System"
 
 char auth[] = "YOUR_BLYNK_TOKEN";
 char ssid[] = "YOUR_WIFI";
@@ -29,14 +30,25 @@ void loop() {
     if (eyeState == "closed") {
       if (closedStart == 0) {
         closedStart = millis();
+        Blynk.virtualWrite(V1, "Eyes Closed");
+        Blynk.setProperty(V1, "color", "#FFA500");
+        Blynk.setProperty(V1, "label", "Closed");
       } else if (millis() - closedStart > 2000) {
         digitalWrite(BUZZER_PIN, HIGH);
+        Blynk.virtualWrite(V1, "DROWSINESS ALERT!");
+        Blynk.setProperty(V1, "color", "#FF0000");
+        Blynk.setProperty(V1, "label", "DROWSY");
+      } else {
+        Blynk.virtualWrite(V1, "Eyes Closed");
+        Blynk.setProperty(V1, "color", "#FFA500");
+        Blynk.setProperty(V1, "label", "Closed");
       }
     } else {
       closedStart = 0;
       digitalWrite(BUZZER_PIN, LOW);
+      Blynk.virtualWrite(V1, "Eyes Open");
+      Blynk.setProperty(V1, "color", "#00FF00");
+      Blynk.setProperty(V1, "label", "Open");
     }
-
-    Blynk.virtualWrite(V1, eyeState);
   }
 }
