c0b9f32b306da281c556bfbf1655a5b59ce957a5
Sivasubramani K J <sivasubramanikj@gmail.com>
2025-09-26 15:48:44 +0530

    Update main.ino

--- frimware/esp32-devkit/main.ino
+++ frimware/esp32-devkit/main.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
-#define BLYNK_TEMPLATE_ID "TMPL3Hy5PI7Ve"
+
+#define BLYNK_TEMPLATE_ID   "TMPL3Hy5PI7Ve"
 #define BLYNK_TEMPLATE_NAME "Drowsiness Detection System"
 
 char auth[] = "YOUR_BLYNK_TOKEN";
@@ -9,22 +10,30 @@ char pass[] = "YOUR_PASS";
 
 #define BUZZER_PIN 12
 
+HardwareSerial camSerial(2);  
+#define RXD2 16  // DevKit RX -> CAM TX
+#define TXD2 17  // DevKit TX -> CAM RX
+
 String eyeState = "open";
 unsigned long closedStart = 0;
 
 void setup() {
-  Serial.begin(115200);  // UART from ESP32-CAM
+  Serial.begin(115200);  
+  camSerial.begin(115200, SERIAL_8N1, RXD2, TXD2);
+
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(BUZZER_PIN, LOW);
 
   Blynk.begin(auth, ssid, pass);
+
+  Serial.println("ESP32-DevKit Ready");
 }
 
 void loop() {
   Blynk.run();
 
-  if (Serial.available()) {
-    eyeState = Serial.readStringUntil('\n');
+  if (camSerial.available()) {
+    eyeState = camSerial.readStringUntil('\n');
     eyeState.trim();
 
     if (eyeState == "closed") {
